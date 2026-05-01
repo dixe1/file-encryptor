42fb5b2f63572e86c28d526be523e371bf19632e
Bartłomiej Dobrowolski <135438002+dixe1@users.noreply.github.com>
2026-05-01 20:52:11 +0200

    refactor

--- main.cpp
+++ main.cpp
@@ -1,68 +1,86 @@
-#include <print>
-#include <fstream>
 #include <filesystem>
+#include <print>
+#include <optional>
 #include <vector>
 #include <random>
-#include <optional>
-#include <string>
+#include <fstream>
 
-enum Type
-{
-    FROM_FILE,
-    FROM_STRING,
-};
 struct Info
 {
-    std::string buffer;
+    std::vector<char> buffer;
     uint32_t key;
 };
-std::optional<Info> process(const std::string& fileNameOrString, Type type = FROM_STRING, uint32_t key = std::random_device{}())
-{
-    std::vector<char> buffer;
-    std::ifstream inputFile;
-    size_t fileSize{};
-    switch (type)
-    {
-        case FROM_FILE:
-            inputFile.open(fileNameOrString, std::ios::binary);
-            if (!inputFile)
-                return std::nullopt;
 
-            fileSize = std::filesystem::file_size(fileNameOrString);
-            buffer.resize(fileSize);
-            if(!inputFile.read(buffer.data(), fileSize))
-                return std::nullopt;
-            break;
-        case FROM_STRING:
-            fileSize = fileNameOrString.size();
-            buffer.resize(fileSize);
-            std::ranges::copy(fileNameOrString, buffer.begin());
-            break;
-        default:
-            return std::nullopt;
-    }
+void process(std::vector<char>& input, uint32_t key)
+{
     std::mt19937 generator(key);
     std::uniform_int_distribution<unsigned char> dist(0, 255);
 
-    for (size_t i{}; i < fileSize; i++)
+    for (size_t i{}; i < input.size(); i++)
     {
-        buffer[i] ^= dist(generator);
+        input[i] ^= dist(generator);
     }
-
-    std::string bufferStr(buffer.data(), fileSize);
-    return Info{bufferStr, key};
 }
+
+// Files
+std::optional<Info> encryptFile(const std::string& fileName)
+{
+    std::ifstream inputFile(fileName, std::ios::binary);
+    const auto key = std::random_device{}();
+    const auto fileSize = std::filesystem::file_size(fileName);
+
+    std::vector<char> data;
+    data.resize(fileSize);
+    inputFile.read(data.data(), fileSize);
+    process(data, key);
+
+    return Info{data, key};
+}
+std::vector<char> decryptFile(const std::string& fileName, const uint32_t key)
+{
+    std::ifstream inputFile(fileName, std::ios::binary);
+    const auto fileSize = std::filesystem::file_size(fileName);
+
+    std::vector<char> data;
+    data.resize(fileSize);
+    inputFile.read(data.data(), fileSize);
+    process(data, key);
+
+    return data;
+}
+
+// Strings
+std::optional<Info> encryptString(const std::string& str)
+{
+    const auto key = std::random_device{}();
+
+    std::vector<char> data(str.begin(), str.end());
+    process(data, key);
+
+    return Info{data, key};
+}
+std::vector<char> decryptString(const std::string& str, const uint32_t& key)
+{
+    std::vector<char> data(str.begin(), str.end());
+    process(data, key);
+
+    return data;
+}
+
 
 int main()
 {
-    // auto e1 = process("GitHubPrefix.txt", FROM_FILE);
-    // if (!e1)
-    //     std::println("error1");
-    //
-    // auto e2 = process(e1.value().buffer, FROM_STRING, e1.value().key);
-    // if (!e2)
-    //     std::println("error2");
-    //
-    // std::println("{}",e1.value().buffer);
-    // std::println("{}",e2.value().buffer);
+    auto test_1 = encryptFile("GitHubPrefix.txt");
+    process(test_1->buffer, test_1->key);   // Decryption
+    for (auto& i : test_1->buffer)
+        std::print("{}", i);
+
+    std::fstream file("GitHubPrefix.txt");
+    std::ostringstream ostr;
+    ostr << file.rdbuf();
+    auto test_2 = encryptString(ostr.str());
+
+    process(test_2->buffer, test_2->key);   // Decryption
+    for (auto& i : test_2->buffer)
+        std::print("{}", i);
 }
